fba74bc2ca83d988221e9e4e583d65750be038dd
XuTongle <xutongle@gmail.com>
2019-02-15 18:49:29 +0800

    更新

--- src/SignatureGuard.php
+++ src/SignatureGuard.php
@@ -20,6 +20,9 @@ use Laravel\Passport\ClientRepository;
  */
 class SignatureGuard
 {
+    const SIGNATURE_METHOD_HMACSHA1 = 'HMAC-SHA1';
+    const SIGNATURE_METHOD_HMACSHA256 = 'HMAC-SHA256';
+
     /**
      * The user provider implementation.
      *
@@ -55,6 +58,7 @@ class SignatureGuard
      */
     public function user(Request $request)
     {
+        //验证公共请求参数
         if (!$request->has('app_id')) {
             throw new AuthenticationException('Missing app_id parameter.');
         }
@@ -67,40 +71,57 @@ class SignatureGuard
             throw new AuthenticationException('Missing signature parameter.');
         }
 
+        if (!$request->has('signature_method')) {
+            throw new AuthenticationException('Missing signature_method parameter.');
+        }
+
+        if (!$request->has('signature_version')) {
+            throw new AuthenticationException('Missing signature_version parameter.');
+        }
+
+        if (!$request->has('signature_nonce')) {
+            throw new AuthenticationException('Missing signature_nonce parameter.');
+        }
+
         //获取参数
         $params = $request->except(['signature']);
 
-        //获取有效的Client
-        if (($client = $this->clients->findActive($params['app_id'])) == null) {
-            throw new AuthenticationException('App_id is incorrect.');
-        }
         //检查时间戳，误差1分钟
         if ((time() - intval($params['timestamp'])) > 60) {
             throw new AuthenticationException('Client time is incorrect.');
-
         }
-        if ($request->input('signature') != $this->getSignature($request, $params, $client->secret)) {
+
+        //获取有效的 Client
+        if (($client = $this->clients->findActive($params['app_id'])) == null) {
+            throw new AuthenticationException('App_id is incorrect.');
+        }
+
+        if ($request->input('signature') != $this->getSignature($params, $client->secret)) {
             throw new AuthenticationException('Signature verification failed');
         }
         return $client->user;
     }
 
-
     /**
      * Calculate signature for request
      *
-     * @param Request $request Request to generate a signature for
      * @param array $params parameters.
-     *
+     * @param $key
      * @return string
-     *
-     * @throws \RuntimeException
+     * @throws AuthenticationException
      */
-    protected function getSignature(Request $request, array $params, $secret)
+    protected function getSignature(array $params, $key)
     {
         //参数排序
         ksort($params);
         $stringToSign = urlencode(http_build_query($params, null, '&', PHP_QUERY_RFC3986));
-        return base64_encode(hash_hmac('sha1', $stringToSign, $secret, true));
+
+        //签名
+        if ($params['signature_method'] == self::SIGNATURE_METHOD_HMACSHA256) {
+            return base64_encode(hash_hmac('sha256', $stringToSign, $key, true));
+        } elseif ($params['signature_method'] == self::SIGNATURE_METHOD_HMACSHA1) {
+            return base64_encode(hash_hmac('sha1', $stringToSign, $key, true));
+        }
+        throw new AuthenticationException('This signature method is not supported.');
     }
 }
